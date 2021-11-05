2bd05ae512daed4940c4b5f95a151b2df4c64053
solve-the-soph <yhua42@gapp.nthu.edu.tw>
2021-11-05 11:29:39 +0800

    add pic

---
+++ demo.js
@@ -0,0 +1,28 @@
+var numArray = [1,5,2,11,7];
+
+console.log(numArray[0]); // Fist position
+console.log(numArray[1]); // Second position
+
+console.log(numArray[4]); //Last position
+numArray[4] = 18;
+console.log(numArray[4]); // Last position with a different value
+
+console.log(numArray); //Entire array
+
+var students = ['Tom', 'Mary', 'Victor', 'John'];
+
+//Mixed types of data in an array recommended
+var profile = ['TY Chen', 'NTHU', 40, 180, 72]
+
+console.log(profile[0])
+
+//ver name = 'TY Chen ', affiliation = 'NTHU', age = 40;
+ver emptyObj = {}; // An empty object
+
+var profile = {
+	name: 'TY Chen',
+	age:39
+	affiliation: 'NTHU',
+	height: 180,
+	weight:72
+};
